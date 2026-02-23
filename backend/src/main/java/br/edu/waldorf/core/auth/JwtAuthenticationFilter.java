4c977d17a21df7a11b7f74196da6e9f810e440d2
danzeroum <danzeroum@gmail.com>
2026-02-22 21:43:25 -0300

    test

--- backend/src/main/java/br/edu/waldorf/core/auth/JwtAuthenticationFilter.java
+++ backend/src/main/java/br/edu/waldorf/core/auth/JwtAuthenticationFilter.java
@@ -10,9 +10,11 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+
 import java.io.IOException;
 
 @Slf4j
